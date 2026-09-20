16dea2ebd6931be14dead30abf2b0296f20c7863
raze-me <ac.otherside@gmail.com>
2026-09-20 22:20:04 +0530

    fixed content.js entry point in vite config, added informatory readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig} from "vite";
-import { crx} from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
 export default defineConfig({
-    plugins: [crx({manifest})]
+  plugins: [
+    crx({
+      manifest,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        content: "src/content/content.ts",
+      },
+      output: {
+        entryFileNames: (chunk) => {
+          if (chunk.name === "content") {
+            return "content.js";
+          }
+          return "assets/[name]-[hash].js";
+        },
+      },
+    },
+  },
 });
